2fbcc8d190da49b69ffa4e494ff3b407022b61f9
Christopher Nugent <awedeven+github@gmail.com>
2018-09-27 15:27:54 -0700

    Remove code for demo

--- pygame_demo/__main__.py
+++ pygame_demo/__main__.py
@@ -10,17 +10,13 @@ if __name__ == '__main__':
 
     field = pygame.Rect(0, 0, 500, 500)
 
-    bullets = []
     game_exit = False
-    timer = None
-    font = None
     while not game_exit:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_exit = True
         foreground = pygame.Surface((500, 500), pygame.SRCALPHA)
         foreground.fill(pygame.Color('black'))
-        bullet_hitboxes = [b.hitbox() for b in bullets]
 
         screen.fill((60, 70, 90))
         screen.blit(foreground, (20, 20))
