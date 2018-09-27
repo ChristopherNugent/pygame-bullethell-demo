a3bab8a7f38ba4ff9294970632cdc8842f76ccb1
Christopher Nugent <awedeven+github@gmail.com>
2018-09-27 15:34:14 -0700

    Add some comments

--- pygame_demo/__main__.py
+++ pygame_demo/__main__.py
@@ -4,21 +4,24 @@ if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('PyGame demo')
 
+    # Set up some stuff for our window
     width, height = 540, 540
     screen = pygame.display.set_mode((width, height))
     clock = pygame.time.Clock()
 
-    field = pygame.Rect(0, 0, 500, 500)
-
+    # Main event loop
     game_exit = False
     while not game_exit:
         for event in pygame.event.get():
+            # Quit if the exit button is clicked
             if event.type == pygame.QUIT:
                 game_exit = True
+        # Some background stuff
         foreground = pygame.Surface((500, 500), pygame.SRCALPHA)
         foreground.fill(pygame.Color('black'))
 
-        screen.fill((60, 70, 90))
+        # Draw our output to the screen
+        screen.fill((60, 70, 90))   # a gray color
         screen.blit(foreground, (20, 20))
         pygame.display.flip()
 
